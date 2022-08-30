2122d8ccda84f5095e9989b1bb5f7c4ac73ffb47
Igor Khomenko <xomigrek@gmail.com>
2022-08-30 20:13:46 +0300

    Add ProductManager

---
+++ src/test/java/ru/netology/ProductsManager/Repository/ProductRepositoryTest.java
@@ -0,0 +1,28 @@
+package ru.netology.ProductsManager.Repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.ProductsManager.Product;
+
+public class ProductRepositoryTest {
+
+    ProductRepository repo = new ProductRepository();
+    Product product1 = new Product(1, "продукт1", 1);
+    Product product2 = new Product(2, "продукт2", 2);
+    Product product3 = new Product(3, "продукт3", 3);
+    Product product4 = new Product(4, "продукт4", 4);
+    Product product5 = new Product(5, "продукт5", 5);
+
+    @Test
+    public void shouldRemoveById() {
+        repo.save(product1);
+        repo.save(product2);
+        repo.save(product3);
+        repo.save(product4);
+        repo.save(product5);
+        repo.removeById(product1.getId());
+        Product[] expected = {product2, product3, product4, product5};
+        Product[] actual = repo.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
